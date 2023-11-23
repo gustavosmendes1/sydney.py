e631eaf8a7abccff51f0a1f876c299e5f57a3100
gustavosmendes1 <105264313+gustavosmendes1@users.noreply.github.com>
2023-11-23 17:54:52 -0300

    Update constants.py

--- sydney/constants.py
+++ sydney/constants.py
@@ -3,12 +3,12 @@ USER_AGENT = "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTM
 CREATE_HEADERS = {
     "Accept": "application/json",
     "Accept-Language": "en-US,en;q=0.9",
-    "Referer": "https://www.bing.com/copilot",
-    "Sec-Ch-Ua": '"Microsoft Edge";v="119", "Chromium";v="119", "Not?A_Brand";v="8"',
-    "Sec-Ch-Ua-Mobile": "?0",
+    "Referer": "https://edgeservices.bing.com/edgesvc/shell",
+    "Sec-Ch-Ua": '"Microsoft Edge";v="119", "Chromium";v="119", "Not?A_Brand";v="24"',
+    "Sec-Ch-Ua-Mobile": "?0",    
     "Sec-Ch-Ua-Platform": "Windows",
     "Sec-Fetch-Dest": "empty",
-    "Sec-Fetch-Mode": "cors",
+    "Sec-Fetch-Mode": "no-cors",
     "Sec-Fetch-Site": "same-origin",
     "User-Agent": USER_AGENT,
     "X-Edge-Shopping-Flag": "0",
@@ -16,7 +16,7 @@ CREATE_HEADERS = {
 
 CHATHUB_HEADERS = {
     "Pragma": "no-cache",
-    "Origin": "https://www.bing.com",
+    "Origin": "https://edgeservices.bing.com",
     "Accept-Language": "en-US,en;q=0.9",
     "User-Agent": USER_AGENT,
     "Cache-Control": "no-cache",
@@ -30,10 +30,10 @@ KBLOB_HEADERS = {
     "User-Agent": USER_AGENT,
 }
 
-BUNDLE_VERSION = "1.1342.3-cplt.7"
+BUNDLE_VERSION = "1.1359.7"
 
 BING_CREATE_CONVERSATION_URL = (
-    f"https://www.bing.com/turing/conversation/create?bundleVersion={BUNDLE_VERSION}"
+    f"https://edgeservices.bing.com/edgesvc/turing/conversation/chats?bundleVersion={BUNDLE_VERSION}"
 )
 BING_GET_CONVERSATIONS_URL = "https://www.bing.com/turing/conversation/chats"
 BING_CHATHUB_URL = "wss://sydney.bing.com/sydney/ChatHub"
